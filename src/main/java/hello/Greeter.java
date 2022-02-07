273f9b049690520f9131123aac63eb46d612b564
Jerzy Krolak <jerzy.krolak@roche.com>
2022-02-07 12:18:08 +0100

    000 a bit of new code

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -3,8 +3,13 @@ package hello;
 public class Greeter {
 
     public static final String HELLO_WORLD = "Hello world!";
+    public static final String SOMETHING_ELSE = "something else";
 
     public String sayHello() {
         return HELLO_WORLD;
+    }
+
+    public String saySomethingElse() {
+        return SOMETHING_ELSE;
     }
 }
